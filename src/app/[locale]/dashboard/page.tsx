3fa8ea26d89ccddbba3c37227952baec875b57f5
Aymenbachiri <aymen.bachiri99@gmail.com>
2025-06-10 15:48:03 +0200

    add metadata to dasboard page

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,5 +1,23 @@
 import { requireAuthentication } from "@/lib/helpers/helpers";
-import { getLocale, setRequestLocale } from "next-intl/server";
+import { getLocale, getTranslations, setRequestLocale } from "next-intl/server";
+import type { Metadata } from "next";
+import type { Locale } from "next-intl";
+
+type Props = { params: Promise<{ locale: Locale }> };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale } = await params;
+
+  const t = await getTranslations({
+    locale,
+    namespace: "DashboardPage.DashboardPageMetadata",
+  });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 
 export default async function page(): Promise<React.JSX.Element> {
   const locale = await getLocale();
