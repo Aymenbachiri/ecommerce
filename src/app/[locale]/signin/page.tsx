c477b1a93d61cdfab80d325f44187802b84ccc64
Aymenbachiri <aymen.bachiri99@gmail.com>
2025-06-10 15:38:11 +0200

    add metadata to signin page

--- src/app/[locale]/signin/page.tsx
+++ src/app/[locale]/signin/page.tsx
@@ -1,5 +1,23 @@
 import { redirectIfAuthenticated } from "@/lib/helpers/helpers";
-import { getLocale, setRequestLocale } from "next-intl/server";
+import type { Metadata } from "next";
+import type { Locale } from "next-intl";
+import { getLocale, getTranslations, setRequestLocale } from "next-intl/server";
+
+type Props = { params: Promise<{ locale: Locale }> };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale } = await params;
+
+  const t = await getTranslations({
+    locale,
+    namespace: "SigninPage.SigninPageMetadata",
+  });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 
 export default async function page(): Promise<React.JSX.Element> {
   const locale = await getLocale();
