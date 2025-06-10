2ef570972be91365aaf49595312a273bdda139ac
Aymenbachiri <aymen.bachiri99@gmail.com>
2025-06-10 09:47:50 +0200

    translate stats

--- src/components/home/stats.tsx
+++ src/components/home/stats.tsx
@@ -1,15 +1,23 @@
 "use client";
+import { useTranslations } from "next-intl";
 import { fadeInUp, staggerContainer } from "./animation";
 import { motion } from "motion/react";
 
-const stats = [
-  { number: "99.9%", label: "Uptime" },
-  { number: "50ms", label: "Response Time" },
-  { number: "10K+", label: "Products Supported" },
-  { number: "24/7", label: "Support" },
+type Stats = {
+  number: string;
+  label: "uptime" | "response_time" | "products_supported" | "support";
+};
+
+const stats: Stats[] = [
+  { number: "99.9%", label: "uptime" },
+  { number: "50ms", label: "response_time" },
+  { number: "10K+", label: "products_supported" },
+  { number: "24/7", label: "support" },
 ];
 
 export function Stats(): React.JSX.Element {
+  const t = useTranslations("Stats");
+
   return (
     <section className="bg-muted/50 border-y py-12">
       <div className="container mx-auto px-4">
@@ -25,7 +33,7 @@ export function Stats(): React.JSX.Element {
               <div className="text-primary mb-2 text-3xl font-bold md:text-4xl">
                 {stat.number}
               </div>
-              <div className="text-muted-foreground">{stat.label}</div>
+              <div className="text-muted-foreground">{t(stat.label)}</div>
             </motion.div>
           ))}
         </motion.div>
