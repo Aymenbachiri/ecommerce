29e7d3ede8969daf737094ad8d47beb587b392e9
Aymenbachiri <aymen.bachiri99@gmail.com>
2025-06-22 10:17:51 +0200

    add generateStaticParams

--- src/app/[locale]/products/[id]/page.tsx
+++ src/app/[locale]/products/[id]/page.tsx
@@ -1,17 +1,17 @@
 import { getLocale, setRequestLocale } from "next-intl/server";
 import { ProductPage } from "./_components/product-page";
-import type { Metadata } from "next";
 import { getProduct } from "./_lib/get-product";
 import { API_URL } from "@/lib/env/env";
-import { ProductWithRelations } from "@/lib/types/types";
+import type { Metadata } from "next";
+import type { ProductWithRelations } from "@/lib/types/types";
 
-type Props = {
-  params: Promise<{ id: string }>;
-};
+type Props = { params: Promise<{ id: string }> };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { id } = await params;
   const product = await getProduct(id);
+
+  if (!product) return { title: "Product Not Found" };
 
   return {
     title: product?.name || "",
@@ -19,13 +19,15 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-// export async function generateStaticParams(): Promise<{ id: string }[]> {
-//   const products = await fetch(`${API_URL}/api/products`).then((res) =>
-//     res.json(),
-//   );
+export async function generateStaticParams(): Promise<{ id: string }[]> {
+  const products = await fetch(`${API_URL}/api/products`).then((res) =>
+    res.json(),
+  );
 
-//   return products.map((product: ProductWithRelations) => ({ id: product.id }));
-// }
+  return products.data.map((product: ProductWithRelations) => ({
+    id: product.id,
+  }));
+}
 
 export default async function page(): Promise<React.JSX.Element> {
   const locale = await getLocale();
