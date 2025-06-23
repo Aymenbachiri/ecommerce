e51ed71149f08afd71c2463efbdcf7b1d2e63863
Aymenbachiri <aymen.bachiri99@gmail.com>
2025-06-23 10:48:36 +0200

    update scalar api url

--- src/app/[locale]/reference/route.ts
+++ src/app/[locale]/reference/route.ts
@@ -5,7 +5,6 @@ const config = {
   cdn: "https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.31.11",
   darkMode: true,
   showSidebar: true,
-  baseServerUrl: process.env.NEXT_PUBLIC_API_URL,
 };
 
 export const GET = ApiReference(config);
